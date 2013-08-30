4e20327b4a3890c69c1dea2ac573001e9c87ed40
Ian Coffey <ian.coffey@gmail.com>
2013-08-30 10:31:41 -0400

    only supports ubuntu

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,6 @@ description      'ghetto little cookbook to play with circus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+supports 'ubuntu'
+
 depends 'python'
